93bc495ec32dbc7c54c29e01ba633f61d93c9110
pishi <pishi@gmail.com>
2022-07-16 16:11:10 +0800

    init

---
+++ SimpleFactory/src/com/pishi/simplefactory/SimpleFactory.java
@@ -0,0 +1,10 @@
+package com.pishi.simplefactory;
+
+/**
+ * @author pishi
+ * @description: 简单工厂
+ * @date 2022年07月16日 12:25
+ */
+public class SimpleFactory {
+
+}
---
+++ SimpleFactory/src/com/pishi/simplefactory/vo/MessageVo.java
@@ -0,0 +1,52 @@
+package com.pishi.simplefactory.vo;
+
+import java.util.Date;
+
+/**
+ * @author pishi
+ * @description: 消息父类
+ * @date 2022年07月16日 12:26
+ */
+public class MessageVo {
+
+    /**
+     * 业务唯一主键
+     */
+    private String uniqueKey;
+
+    /**
+     * 发送时间
+     */
+    private Date sendTime;
+
+
+    /**
+     * 操作标识
+     */
+    private String operateFlag;
+
+
+    public String getUniqueKey() {
+        return uniqueKey;
+    }
+
+    public void setUniqueKey(String uniqueKey) {
+        this.uniqueKey = uniqueKey;
+    }
+
+    public Date getSendTime() {
+        return sendTime;
+    }
+
+    public void setSendTime(Date sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public String getOperateFlag() {
+        return operateFlag;
+    }
+
+    public void setOperateFlag(String operateFlag) {
+        this.operateFlag = operateFlag;
+    }
+}
