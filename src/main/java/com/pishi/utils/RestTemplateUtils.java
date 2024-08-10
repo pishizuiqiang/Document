9db7ad371266f25a7a24c5b492d47b98443f97d0
pishi <pixiuchun@foxmail.com>
2024-08-10 15:57:39 +0800

    doc20240530 泛型参数获取

--- src/main/java/com/pishi/utils/RestTemplateUtils.java
+++ src/main/java/com/pishi/utils/RestTemplateUtils.java
@@ -15,7 +15,7 @@ import java.util.Optional;
 public class RestTemplateUtils {
 
 
-    private static final String URL = "http://iron.bxgt.com/module/analysis/";
+    private static final String URL = "http://xxxx/module/analysis/";
 
 
     /**
